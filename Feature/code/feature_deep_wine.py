996a024cb2b5dd8046d3a46ed22154a49179b75b
Jialh2118 <jialh2118@gmail.com>
2023-06-13 21:15:22 +0800

    Add files via upload

---
+++ Feature/code/feature_deep_wine.py
@@ -0,0 +1,281 @@
+# import sys; print('Python %s on %s' % (sys.version, sys.platform))
+# sys.path.extend(['C:\\JiaLH\\project\\LAMDA-SSL\\LAMDA-SSL', 'C:/JiaLH/project/LAMDA-SSL/LAMDA-SSL'])
+from xgboost import XGBClassifier
+from LAMDA_SSL.Dataset.LabeledDataset import LabeledDataset
+from LAMDA_SSL.Dataset.UnlabeledDataset import UnlabeledDataset
+from LAMDA_SSL.Transform.ToTensor import ToTensor
+from LAMDA_SSL.Dataloader.LabeledDataloader import LabeledDataLoader
+from LAMDA_SSL.Dataloader.UnlabeledDataloader import UnlabeledDataLoader
+from LAMDA_SSL.Sampler.RandomSampler import RandomSampler
+from LAMDA_SSL.Sampler.SequentialSampler import SequentialSampler
+from LAMDA_SSL.Augmentation.Tabular.Noise import Noise
+from LAMDA_SSL.Evaluation.Classifier.Accuracy import Accuracy
+from LAMDA_SSL.Algorithm.Classification.Supervised import Supervised
+from LAMDA_SSL.Network.FT_Transformer import FT_Transformer
+from LAMDA_SSL.Algorithm.Classification.PiModel import PiModel
+from LAMDA_SSL.Algorithm.Classification.TemporalEnsembling import TemporalEnsembling
+from LAMDA_SSL.Algorithm.Classification.UDA import UDA
+from LAMDA_SSL.Algorithm.Classification.PseudoLabel import PseudoLabel
+from LAMDA_SSL.Algorithm.Classification.ImprovedGAN import ImprovedGAN
+from LAMDA_SSL.Algorithm.Classification.ICT import ICT
+from LAMDA_SSL.Algorithm.Classification.MixMatch import MixMatch
+from LAMDA_SSL.Algorithm.Classification.FixMatch import FixMatch
+from LAMDA_SSL.Algorithm.Classification.FlexMatch import FlexMatch
+from LAMDA_SSL.Algorithm.Classification.SoftMatch import SoftMatch
+from LAMDA_SSL.Opitimizer.Adam import Adam
+from sklearn.datasets import load_wine
+import torch
+import random
+import os
+import argparse
+import csv
+import pickle
+from LAMDA_SSL.Split.DataSplit import DataSplit
+import copy
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from math import ceil
+from LAMDA_SSL.Algorithm.Classification.LabelPropagation import LabelPropagation
+from sklearn.utils import check_random_state
+# p_y=[0.9,0,1]
+# p_y=[0.95,0.85,0.75,0.65,0.55,0.45,0.35,0.25,0.15,0.05]
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    os.environ['PYTHONHASHSEED'] = str(seed)
+parser = argparse.ArgumentParser()
+parser.add_argument('--labels', type=int, default=5)
+parser.add_argument('--device', type=str, default='cuda:0')
+args = parser.parse_args()
+labels=args.labels
+device=args.device
+
+# unlabels=args.unlabels
+# def distribution_selection_y(X,y,p):
+#     r=np.random.random(X.shape[0])
+#     s = []
+#     # print(X.shape)
+#     for _ in range(X.shape[0]):
+#         if r[_]<=p_y[int(y[_])]:
+#             s.append(1)
+#         else:
+#             s.append(0)
+#     s=np.array(s)
+#     s_X=X[s==1]
+#     # print(s_X.shape)
+#     s_y=y[s==1]
+#     # r_s=random.sample(list(range(X.shape[0])),s_X.shape[0])
+#     r_X=X[s==0]
+#     r_y=y[s==0]
+#     return s_X,s_y,r_X,r_y
+
+def distribution_selection_condition(X,y,p=0.2,interval=0.2,num_classes=3):
+    source_X_list=[]
+    target_X_list=[]
+    source_y_list=[]
+    target_y_list=[]
+    for _ in range(num_classes):
+        _X = X[y==_]
+        _y = y[y==_]
+        mean_X=np.mean(_X,axis=0)
+        dis=[]
+        for _ in range(_X.shape[0]):
+            s = np.linalg.norm(mean_X - _X[_])
+            dis.append(s)
+        dis=np.array(dis)
+        index=dis.argsort()
+        source_index, target_index = index[:ceil(index.shape[0] * 0.5)], index[ceil(index.shape[0] * 0.5):]
+        # print(source_index.shape)
+        # print(target_index.shape)
+        # print(target_index[ceil(target_index.shape[0] * (p-interval)):ceil(target_index.shape[0] * p)].shape)
+        # print(ceil(target_index.shape[0] * (p-interval)))
+        # print(ceil(target_index.shape[0] * p))
+        source_X_list.append(_X[source_index])
+        source_y_list.append(_y[source_index])
+        target_X_list.append(_X[target_index[ceil(target_index.shape[0] * (p-interval)):ceil(target_index.shape[0] * p)]])
+        target_y_list.append(_y[target_index[ceil(target_index.shape[0] * (p-interval)):ceil(target_index.shape[0] * p)]])
+    source_X = np.concatenate((source_X_list))
+    source_y = np.concatenate((source_y_list))
+    target_X = np.concatenate((target_X_list))
+    target_y = np.concatenate((target_y_list))
+    return source_X, source_y,target_X,target_y
+
+def feature_selection(labeled_X,unlabeled_X,p=0.5,random_state=None):
+    rng = check_random_state(seed=random_state)
+    permutation = rng.permutation(unlabeled_X.shape[1])
+    mask_feature = permutation[:ceil(unlabeled_X.shape[1]*p)]
+    unlabeled_X[:,mask_feature]=labeled_X.mean(axis=0)[mask_feature]
+    return  unlabeled_X
+
+
+evaluation= Accuracy()
+
+labeled_dataset=LabeledDataset(transform=ToTensor())
+unlabeled_dataset=UnlabeledDataset(transform=ToTensor())
+test_dataset=UnlabeledDataset(transform=ToTensor())
+
+labeled_dataloader=LabeledDataLoader(batch_size=64,num_workers=0,drop_last=True)
+unlabeled_dataloader=UnlabeledDataLoader(num_workers=0,drop_last=True)
+test_dataloader=UnlabeledDataLoader(batch_size=64,num_workers=0,drop_last=False)
+
+labeled_sampler=RandomSampler(replacement=True,num_samples=64*(10000))
+unlabeled_sampler=RandomSampler(replacement=True)
+test_sampler=SequentialSampler()
+
+weak_augmentation=Noise(0.1)
+strong_augmentation=Noise(0.2)
+augmentation={
+    'weak_augmentation':weak_augmentation,
+    'strong_augmentation':strong_augmentation
+}
+path='../../data/numerical_only/balanced/'
+rate_list=[0,0.2,0.4,0.6,0.8,1.0]
+f=open("wine"+"_deep"+'_feature_labels_'+str(labels)+'_1.csv', "w", encoding="utf-8")
+r = csv.DictWriter(f,['algorithm','rate','mean','std'])
+X,y=load_wine(return_X_y=True)
+num_classes=len(np.unique(y))
+labels=num_classes*labels
+algorithms = {
+    # 'FT_Transformer': Supervised(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                              test_dataset=test_dataset, device=device, augmentation=None,
+    #                              network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #                              # network=MLPCLS(num_classes=2, dim_in=X.shape[1]),
+    #                              num_it_epoch=1000, labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                              scheduler=None, weight_decay=1e-5),
+
+    # 'ImprovedGAN': ImprovedGAN(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                            test_dataset=test_dataset, device=device, num_it_epoch=100, epoch=10,
+    #                            labeled_sampler=RandomSampler(replacement=True, num_samples=64 * (100)),
+    #                            optimizer=Adam(lr=1e-4),
+    #                            scheduler=None, weight_decay=1e-5),
+
+    # 'PiModel': PiModel(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                    test_dataset=test_dataset, device=device, augmentation=weak_augmentation,
+    #                    network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #                    # network=MLPCLS(num_classes=2, dim_in=X.shape[1]),
+    #                    num_it_epoch=1000, labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                    scheduler=None, weight_decay=1e-5),
+    # 'TemporalEnsembling': TemporalEnsembling(labeled_dataset=labeled_dataset,
+    #                                          unlabeled_dataset=unlabeled_dataset, test_dataset=test_dataset,
+    #                                          device=device, augmentation=weak_augmentation,
+    #                                          network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #                                          # network=MLPCLS(num_classes=2,dim_in=X.shape[1]),
+    #                                          num_it_epoch=100, epoch=10,
+    #                                          labeled_sampler=RandomSampler(replacement=True, num_samples=64 * (100)),
+    #                                          optimizer=Adam(lr=1e-4),
+    #                                          scheduler=None, weight_decay=1e-5),
+    # 'UDA': UDA(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset, test_dataset=test_dataset,
+    #            device=device, augmentation=augmentation, network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #            num_it_epoch=1000, labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #            scheduler=None, weight_decay=1e-5),
+    # 'PseudoLabel': PseudoLabel(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                            test_dataset=test_dataset, device=device, augmentation=weak_augmentation,
+    #                            network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #                            num_it_epoch=1000, labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                            scheduler=None, weight_decay=1e-5),
+    # 'ICT': ICT(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset, test_dataset=test_dataset,
+    #            device=device, augmentation=weak_augmentation, network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes),
+    #            num_it_epoch=1000, labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #            scheduler=None, weight_decay=1e-5),
+    # 'MixMatch': MixMatch(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                      test_dataset=test_dataset, device=device, augmentation=weak_augmentation,
+    #                      network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes), num_it_epoch=1000,
+    #                      labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                      scheduler=None, weight_decay=1e-5),
+    # 'FixMatch': FixMatch(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                      test_dataset=test_dataset, device=device, augmentation=augmentation,
+    #                      network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes), num_it_epoch=1000,
+    #                      labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                      scheduler=None, weight_decay=1e-5),
+    # 'FlexMatch': FlexMatch(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+    #                        test_dataset=test_dataset, device=device, augmentation=augmentation,
+    #                        network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes), num_it_epoch=1000,
+    #                        labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+    #                        scheduler=None, weight_decay=1e-5)    
+    'SoftMatch': SoftMatch(labeled_dataset=labeled_dataset, unlabeled_dataset=unlabeled_dataset,
+                           test_dataset=test_dataset, device=device, augmentation=augmentation,
+                           network=FT_Transformer(dim_in=X.shape[1], num_classes=num_classes), num_it_epoch=1000,
+                           labeled_sampler=labeled_sampler, optimizer=Adam(lr=1e-4),
+                           scheduler=None, weight_decay=1e-5)
+}
+
+for name,algorithm in algorithms.items():
+    print(name)
+    for rate in rate_list:
+        print(rate)
+        performance_list = []
+        performance_list_r = []
+        for _ in range(5):
+            set_seed(_)
+            # _labeled_X, _labeled_y, unlabeled_X, unlabeled_y=distribution_selection(X,y,0.8,_)
+            # test_X, test_y, labeled_X, labeled_y = DataSplit(stratified=True, shuffle=True,
+            #                                              random_state=_, X=_labeled_X, y=_labeled_y, size_split=0.4)
+            # trans = StandardScaler().fit(np.concatenate((labeled_X,unlabeled_X)))
+            # test_X = trans.transform(test_X)
+            # labeled_X=trans.transform(labeled_X)
+            # unlabeled_X = trans.transform(unlabeled_X)
+            source_X, source_y, target_X, target_y = DataSplit(stratified=True, shuffle=True, random_state=_, X=X, y=y, size_split=0.5)
+
+            # print(target_X.shape)
+            # print(target_y.shape)
+            labeled_X, labeled_y, test_X, test_y = DataSplit(stratified=True, shuffle=True, random_state=_, X=source_X, y=source_y, size_split=labels)
+
+            unlabeled_X = feature_selection(labeled_X,target_X,rate,random_state=_)
+            unlabeled_y=target_y
+            # trans = StandardScaler().fit(train_X)
+            # test_X = trans.transform(test_X)
+            # train_X=trans.transform(train_X)
+            #
+            # labeled_X, labeled_y, unlabeled_X, unlabeled_y = DataSplit(stratified=True, shuffle=True,
+            #                                                            random_state=_, X=_train_X,
+            #                                                            y=_train_y, size_split=labels)
+            # unlabeled_X, unlabeled_y = distribution_selection_condition(unlabeled_X, unlabeled_y, p=rate, seed=_)
+            # unlabeled_X, unlabeled_y = random_selection(unlabeled_X, unlabeled_y)
+            # print(unlabeled_X.shape)
+
+            # print(unlabeled_X.shape)
+            trans = StandardScaler().fit(labeled_X)
+            test_X = trans.transform(test_X)
+            labeled_X=trans.transform(labeled_X)
+            trans = StandardScaler().fit(unlabeled_X)
+            unlabeled_X = trans.transform(unlabeled_X)
+            algorithm_1=copy.deepcopy(algorithm)
+            pred_y = algorithm_1.fit(labeled_X, labeled_y, unlabeled_X).predict(test_X)
+            performance = Accuracy().scoring(test_y, pred_y)
+            performance_list.append(performance)
+            #performance_r = Accuracy().scoring(test_y, pred_y_1)
+            #performance_list_r.append(performance_r)
+            print(performance)
+            #print(performance_r)
+        performance_list=np.array(performance_list)
+        mean=performance_list.mean()
+        std=performance_list.std()
+        d={}
+        d['algorithm']=name
+        d['mean']=mean
+        d['std']=std
+        d['rate']=rate
+        print(d)
+        r.writerow(d)
+        f.flush()
+        #performance_list_r=np.array(performance_list_r)
+        #mean_r=performance_list_r.mean()
+        #std_r=performance_list_r.std()
+        #d={}
+        #d['algorithm']=name+'random'
+        #d['mean']=mean_r
+        #d['std']=std_r
+        #d['rate']=rate
+        #print(d)
+        #r.writerow(d)
+f.close()
+
+# print(dataset)
+# print('end!')
---
+++ Feature/draw/draw_deep_feature.py
@@ -0,0 +1,215 @@
+import csv
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 10
+import numpy as np
+from scipy.stats import pearsonr
+# dataset="wine"
+labels=5
+datasets=[
+        "iris",
+        "wine",
+        "letter",
+        "covertype"]
+        # "jannis",
+        # "helna",
+        # "aloi"
+        # "wine",
+        # "eye_movements",
+        # "phoneme"
+        # "pol",
+        # "kdd_ipums_la_97-small",
+        # "bank-marketing",
+        # "MagicTelescope",
+        # "house_16H",
+        # "credit",
+        # "california",
+        # "electricity",
+        # "jannis",
+        # "MiniBooNE"
+#    ]
+# domain=['amazon','dslr','webcam']
+
+methods = {
+        # "FT_Transformer" : {
+        #     "symbol": "-",
+        #     "color": "dimgray",
+        #     "reveal": "FT_Transformer"
+        # },
+        "FT_Transformer" : {
+            "symbol": "-",
+            "color": "dimgray",
+            "reveal": "Supervised"
+        },
+        "PseudoLabel": {
+            "symbol": "8-",
+            "color": "#33A02C",
+            "reveal": "PseudoLabel"
+        },
+        # "ImprovedGAN" : {
+        #     "symbol": ".-",
+        #     "color": "goldenrod",
+        #     "reveal": "ImprovedGAN"
+        # },
+        "PiModel":{
+            "symbol": "v-",
+            "color": "#D62728",
+            "reveal": "PiModel"
+        },
+        "ICT": {
+            "symbol": "h-",
+            "color": "#F4C20D",
+            "reveal": "ICT"
+        },
+        # "TemporalEnsembling":{
+        #     "symbol": "^-",
+        #     "color": "teal",
+        #     "reveal": "TemporalEnsembling"
+        # },
+        "UDA":{
+            "symbol": "D-",
+            "color": "#911EB4",
+            "reveal": "UDA"
+        },
+        # "MixMatch": {
+        #     "symbol": "H-",
+        #     "color": "purple",
+        #     "reveal": "MixMatch"
+        # },
+        "FixMatch": {
+            "symbol": "*-",
+            "color": "#46F0F0",
+            "reveal": "FixMatch"
+        },
+        "FlexMatch": {
+            "symbol": "s-",
+            "color": "orange",
+            "reveal": "FlexMatch"
+        },
+        "SoftMatch": {
+            "symbol": "s-",
+            "color": "pink",
+            "reveal": "SoftMatch"
+        },
+    }
+
+# labels=100
+rate_list=[str(0),str(0.2),str(0.4),str(0.6),str(0.8),str(1.0)]
+rate_list_f=[0,0.2,0.4,0.6,0.8,1.0]
+fig, axes = plt.subplots(1, 4, figsize=(10, 3))
+fig.suptitle('Tabular data'+" with "+str(labels)+" labels per class", fontsize=13)
+# plt.title("Office-31"+"with "+100+" labels", fontdict={"size": 26})
+# plt.xlabel("Class Inconsistency", fontdict={"size": 26})
+def AUC(Acc_T):
+    return np.mean(np.array(Acc_T))
+def Acc_T0(Acc_T):
+    return Acc_T[0]
+def WA(Acc_T):
+    return np.min(np.array(Acc_T))
+
+def EVM(Acc_T):
+    sum_m=0
+    for i in range(len(Acc_T)-1):
+        sum_m+=Acc_T[i]-Acc_T[i+1] if Acc_T[i]>Acc_T[i+1] else Acc_T[i+1]-Acc_T[i]
+    return sum_m/(len(Acc_T)-1)
+def VS(Acc_T):
+    return np.std(np.array([Acc_T[i+1]-Acc_T[i] for i in range(len(Acc_T)-1)]))
+def RCC(Acc_T):
+    return pearsonr(Acc_T,[i*1/(len(Acc_T)-1) for i in range(len(Acc_T))])[0]
+
+g=open('./Feature/'+'deep_tabular'+str(labels)+'_stat.csv', "w",encoding="utf-8")
+r = csv.DictWriter(g,['dataset','algorithm','AUC','Acc_T0','WA','EVM','VS','RCC'])
+
+ 
+
+pos=0
+# for source in domain:
+#     for target in domain:
+#         if source==target:
+#             continue
+for dataset in datasets:
+    f = open("Feature/"+dataset+"_deep_feature_labels_"+str(labels)+'.csv', "r",encoding="utf-8")
+    f_read=csv.reader(f)
+    dict_mean={}
+    dict_std={}
+    for row in f_read:
+        # print(row)
+        print(row)
+        if row[1]=='1':
+            row[1]='1.0'
+        if row[0]=='FT_Transformer' and row[1]!='0':
+            # print(row)
+            dict_mean[row[0]+str(row[1])]=dict_mean[row[0]+'0']
+            dict_std[row[0]+str(row[1])]=dict_std[row[0]+'0']
+        # elif row[3]=='0':
+        #     _d=domain[0] if row[1]!=domain[0] else domain[1]
+        #     dict_mean[row[0]+str(row[1])+str(row[2])+str(row[3])]=dict_mean[row[0]+str(row[1])]
+        #     dict_std[row[0]+str(row[1])+str(row[2])+str(row[3])]=dict_std[row[0]+str(row[1])]
+        # else:
+        else:
+            dict_mean[row[0]+str(row[1])]=float(row[2])
+            dict_std[row[0]+str(row[1])] = float(row[3])  
+    i=pos//4
+    j=pos%4
+    # max_mean=0
+    # min_mean=100
+    max_mean=min(dict_mean['FT_Transformer' +'0']+0.1,1)
+    min_mean=max(dict_mean['FT_Transformer' +'0']-0.1,0)
+    for method in methods:
+        color = methods[method]["color"]
+        symbol = methods[method]["symbol"]
+        reveal = methods[method]["reveal"]
+        mean_list=[]
+        std_list = []
+        d={}
+        for rate in rate_list:
+            # if rate==0:
+            #     mean_list.append(dict_mean[method +source+target+ '0'])
+            #     std_list.append(dict_std[method + source+target+'0'])
+            # elif rate==1:
+            #     mean_list.append(dict_mean[method +source+target+ '1'])
+            #     std_list.append(dict_std[method + source+target+'1'])
+            # else:
+            # max_mean=max(max_mean,dict_mean[method+str(rate)])
+            # min_mean=min(min_mean,dict_mean[method+str(rate)])
+            mean_list.append(dict_mean[method+str(rate)])
+            std_list.append(dict_std[method+str(rate)])
+        y=np.array(mean_list)
+        # print(y)
+        y=y*100
+        y_std=np.array(std_list)
+        # print(y_std)
+        y_std=y_std*100/2
+        X=rate_list_f
+        axes[j].plot(X, y, symbol, label=reveal, color=color)
+        d['dataset']=dataset
+        d['algorithm']=method
+        d['AUC']="{:.3f}".format(AUC(mean_list))
+        d['Acc_T0']="{:.3f}".format(Acc_T0(mean_list))
+        d['WA']="{:.3f}".format(WA(mean_list))
+        d['EVM']="{:.3f}".format(EVM(mean_list))
+        d['VS']="{:.3f}".format(VS(mean_list))
+        d['RCC']="{:.3f}".format(RCC(mean_list))
+        r.writerow(d)
+        # plt.fill_between(X, y - y_std, y + y_std, color=color, alpha=0.2)
+
+        # print(i)
+        # print(j)
+    axes[j].legend(loc="lower left")
+    axes[j].set_xlabel("Feature Space t", fontdict={"size": 8})
+    axes[j].set_ylabel("$Acc_T(t) (\%)$", fontdict={"size": 8})
+    axes[j].grid()
+    axes[j].set_xticks(rate_list_f)
+    axes[j].set_yticks([min_mean*100+(max_mean-min_mean)*i*10 for i in range(0,11,1)])
+    axes[j].set_title('Dataset: '+dataset,fontdict={"size": 8})
+    # axes[i,j].savefig('iris' + '_deep' + '_class' + '_labels_' + str(labels)+'.png' , dpi=300)
+    # axes[i,j].show()
+    pos+=1
+plt.tight_layout()
+plt.savefig('Feature_deep_'+'tabular'+'_labels_'+str(labels)+'.eps',dpi=300)
+plt.savefig('Feature_deep_'+'tabular'+'_labels_'+str(labels)+'.png',dpi=300)
+plt.show()
+
+
+
+
